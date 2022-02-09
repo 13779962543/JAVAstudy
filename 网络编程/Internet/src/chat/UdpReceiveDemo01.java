bb7c850a345e01a6ef37154f382788f196c05e91
13779962543 <1507543567@qq.com>
2022-02-09 22:22:29 +0800

    Interest学习

---
+++ 网络编程/Internet/src/chat/UdpReceiveDemo01.java
@@ -0,0 +1,28 @@
+package chat;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class UdpReceiveDemo01 {
+    public static void main(String[] args)throws Exception{
+        DatagramSocket socket=new DatagramSocket(6666);
+
+        //准备接收包裹
+        while(true){
+
+            byte[] container=new byte[1024];
+
+            DatagramPacket packet=new DatagramPacket(container,0,container.length);
+            //断开连接 bye
+            byte[] data=packet.getData();
+            String receiveData=new String(data,0,data.length);
+            System.out.println(receiveData);
+            if(receiveData.equals("bye")){
+                break;
+            }
+
+             }
+        socket.close();
+    }
+
+}
