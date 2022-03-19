cd22cd932abed2a800ac85bf6b3986e4391deb2a
13779962543 <1507543567@qq.com>
2022-03-19 18:24:26 +0800

    二叉树

---
+++ 数据结构学习/src/test01/tree/BinaryTreeDemo.java
@@ -0,0 +1,16 @@
+package test01.tree;
+
+
+import com.sun.source.tree.BinaryTree;
+
+public class BinaryTreeDemo {
+    public static void main(String[] args) {
+        //先创建一个二叉树
+        BinaryTreebinaryTree=new BinaryTree();
+        //创建需要的结点
+
+
+
+
+    }
+}
