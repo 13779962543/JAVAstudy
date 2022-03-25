71cf0ce3de2c533ebc76248bfec34de6a926f346
13779962543 <1507543567@qq.com>
2022-03-25 16:50:30 +0800

    movieapp

--- JAVA基础学习/src/movieapp/run/MovieSystem.java
+++ JAVA基础学习/src/movieapp/run/MovieSystem.java
@@ -3,9 +3,10 @@ package movieapp.run;
 import movieapp.bean.Business;
 import movieapp.bean.Customer;
 import movieapp.bean.Movie;
+import movieapp.bean.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import movieapp.bean.User;
+
 
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -107,9 +108,9 @@ public class MovieSystem {
                     login();
                     break;
                 case "2":
-                    //
                     break;
                 case "3":
+                    showBusinessMain();
                     break;
                 default:
                     System.out.println("命令有误，请确认！");
