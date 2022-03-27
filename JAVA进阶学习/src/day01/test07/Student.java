3d1be8e8ff8d5928438c49852897f8f301171f2f
13779962543 <1507543567@qq.com>
2022-03-27 19:48:13 +0800

    day01

---
+++ JAVA进阶学习/src/day01/test07/Student.java
@@ -0,0 +1,8 @@
+package day01.test07;
+
+public class Student extends People {
+    // 特有功能
+    public void study(){
+        System.out.println(getName()+"学生认真学习~~~");
+    }
+}
---
+++ JAVA进阶学习/src/day01/test07/Teacher.java
@@ -0,0 +1,8 @@
+package day01.test07;
+
+public class Teacher extends People {
+    // 特有功能
+    public void teach(){
+        System.out.println(getName()+"老师要授课");
+    }
+}
